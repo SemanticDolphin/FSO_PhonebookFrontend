79c78298687a3aee9b28ab5cda618c689f69a142
SemanticDolphin <e.andersen@tuta.io>
2023-03-20 16:53:18 +0100

    Added proxy and changed the db url fetching thingy so it works in dev and prod

--- src/services/persons.js
+++ src/services/persons.js
@@ -1,25 +1,26 @@
-import axios from "axios"
+import axios from "axios";
 
-const SERVER_URL = 'http://localhost:3001/persons'
+// const SERVER_URL = "http://localhost:3001/api/persons"; //Local Version
+// const SERVER_URL = "https://render-test-tjdg.onrender.com/api/persons"; //Current online Version
+const SERVER_URL = "/api/persons"; //Current online Version
 
 const getAll = () => {
-  const request = axios.get(SERVER_URL)
-  return request.then(response => response.data)
-}
+  const request = axios.get(SERVER_URL);
+  return request.then((response) => response.data);
+};
 
 const create = (newObject) => {
-  const request = axios.post(SERVER_URL, newObject)
-  return request.then(response => response.data)
-}
+  const request = axios.post(SERVER_URL, newObject);
+  return request.then((response) => response.data);
+};
 const removePerson = (personId) => {
-  const request = axios.delete(`${SERVER_URL}/${personId}`)
-  return request // Probably not needed to extract the response data as it is just an empty object
-}
+  const request = axios.delete(`${SERVER_URL}/${personId}`);
+  return request; // Probably not needed to extract the response data as it is just an empty object
+};
 
 const updateNumber = (personId, newObject) => {
-  const request = axios.put(`${SERVER_URL}/${personId}`, newObject)
-  return request.then(response => response.data)
+  const request = axios.put(`${SERVER_URL}/${personId}`, newObject);
+  return request.then((response) => response.data);
+};
 
-}
-
-export default { getAll, create, removePerson, updateNumber }
+export default { getAll, create, removePerson, updateNumber };
